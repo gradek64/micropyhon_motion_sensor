f39eeeaa8a0ac940ae6e38abc0bd48d6750960dc
Gregi <gregi@Gregis-MacBook-Pro.local>
2023-07-21 18:15:31 +0100

    do not use recursive it has limit use while loop instead

--- prod/Sensor_Class.py
+++ prod/Sensor_Class.py
@@ -1,14 +1,5 @@
 from time import sleep
 
-
-
-# from testing_intervals import TestData
-
-# testing remove for actual Pin value once connected
-# testData = TestData()
-# testing remove for actual Pin value once connected
-
-
 class Colors:
     HEADER = "\033[95m"
     OKBLUE = "\033[94m"
@@ -22,7 +13,7 @@ class Colors:
 
 
 class Sensor:
-    # self means attribute
+    # self means private Class attribute
     def __init__(
         self,
         id,
@@ -54,18 +45,7 @@ class Sensor:
         # x intervals check to make 30s all together for example 3 x activeIntervalCheck
         for x in range(activeSeriesNum):
             self.intervalsArray.append(self.activeInterval)
-
-        # testing remove for actual Pin value once connected
-        # self.testingIncrement = 0
-        # testing remove for actual Pin value once connected
         
-        #get ready sounds
-        #self.dog_barking = AudioSegment.from_mp3('sounds/dog-barking.mp3')
-        #self.me_talking = AudioSegment.from_mp3('sounds/me_talking.mp3')
-        
-        #notify with audio that system is ready
-        #audio_notify = AudioSegment.from_mp3('sounds/ready_now.mp3')
-
         #play(audio_notify) when second sensor is engaged
         if self.id == 2:
             self.sounds.playSound('notify_ready')
@@ -74,98 +54,98 @@ class Sensor:
             print('audio notification should have played')
         
     def runTimingAlarm(self):
-        self.HIGH = self.sensorPin.readPinValue()
-        ledOff = not self.detectionLed.readPinValue()
+        while True:
+            self.HIGH = self.sensorPin.readPinValue()
+            ledOff = not self.detectionLed.readPinValue()
 
-        if self.HIGH == True:
-            sleep(1)
-            # testing
-            # self.testingIncrement += 1
-            # testing
-            
-            # sensor High set Led on if Not already on
-            if ledOff:
-                self.detectionLed.writeOutPin('HIGH')
+            if self.HIGH == True:
+                # check pin ON every second
+                sleep(1)
                 
-            self.beenHighTime += 1
-            if self.debug:
-                print(f"{Colors.WARNING} Pin {self.sensorPin.readPinName()} been High for: {self.beenHighTime}{Colors.ENDC}")
-            
-            # Pin hits check points secends in intervalsArray
-            if self.beenHighTime == self.intervalsArray[self.activeCheckTimes]:
-                self.activeCheckTimes += 1
-                # reset beenHighTime for easier calculation based on activeCheckTimes
-                self.beenHighTime = 0
-                # reset counting for delay
-                self.beenLowDelayCheck = 0
+                # sensor High, set Led ON if Not already ON
+                if ledOff:
+                    self.detectionLed.writeOutPin('HIGH')
+                    
+                self.beenHighTime += 1
                 if self.debug:
-                    print(f"{Colors.OKCYAN} Pin {self.sensorPin.readPinName()}  activeCheckTimes :---: {self.activeCheckTimes} :----:")
+                    print(f"{Colors.WARNING} Pin {self.sensorPin.readPinName()} been High for: {self.beenHighTime}{Colors.ENDC}")
                 
-                # play warning audio when all intervals are greater than 1
-                if self.activeSeriesNum !=1 and self.activeCheckTimes == len(self.intervalsArray) -1:
-                    if self.debug:
-                       print('dog barking warning')
-                     
-                    #start warning audio
-                    self.sounds.playSound('dog_barking')
-
-                if self.activeCheckTimes == len(self.intervalsArray):
-                    if self.debug:
-                        print(f"{Colors.FAIL}!!! Pin {self.sensorPin.readPinName()} ALARM SET !!!!{Colors.ENDC}")
-                        print(f"{Colors.FAIL}!!! me shouting")
-                     
-                    #start warning audio not sure yet
-                    #play(self.me_talking)
-                    self.sounds.playSound('me_talking')
-                    
-                    # set relay pin on for alarm if not already on
-                    relayOff = not self.relayOutput1.readPinValue()
-                    if relayOff:
-                        if self.debug:
-                            print(f"{Colors.FAIL}!!! ===> relay set ON")
-                        self.relayOutput1.writeOutPin('HIGH')
-                    
-                    # alarm is on for 10s
-                    # maybe check how many times alarm got off and decrease time for next check ?
-                    sleep(self.alarmIsOnFor)
-                    #turn off the relay
-                    if self.debug:
-                        print(f"{Colors.FAIL}!!! ===> relay set OFF")
-                    self.relayOutput1.writeOutPin('LOW')
-                    # reset everything
+                # Pin hits check points secends in intervalsArray
+                if self.beenHighTime == self.intervalsArray[self.activeCheckTimes]:
+                    self.activeCheckTimes += 1
+                    # reset beenHighTime for easier calculation based on activeCheckTimes
                     self.beenHighTime = 0
-                    self.activeCheckTimes = 0
+                    # reset counting for delay
+                    self.beenLowDelayCheck = 0
+                    if self.debug:
+                        print(f"{Colors.OKCYAN} Pin {self.sensorPin.readPinName()}  activeCheckTimes :---: {self.activeCheckTimes} :----:")
                     
-            # run method in next second
-            self.runTimingAlarm()
-        else:
-            sleep(1)
-            # testing
-            # self.testingIncrement = self.testingIncrement + 1
-            # testing
-            
-             # sensor Low set Led off if Not already off
-            if not ledOff:
-                self.detectionLed.writeOutPin('LOW')
-                
-            # always reset beenHighTime cause activeCheckTimes tracks if was active before
-            self.beenHighTime = 0
+                    # play warning audio when all intervals are greater than 1
+                    if self.activeSeriesNum !=1 and self.activeCheckTimes == len(self.intervalsArray) -1:
+                        if self.debug:
+                            print('dog barking warning')
+                        
+                        #start warning audio
+                        self.sounds.playSound('dog_barking')
 
-            if self.activeCheckTimes > 0:
-                self.beenLowDelayCheck += 1
-                if self.debug:
-                    print(f"{Colors.OKGREEN} Pin {self.sensorPin.readPinName()} been Low for: {self.beenLowDelayCheck}{Colors.ENDC}")
-            if self.beenLowDelayCheck > self.delayCheck:
-                self.beenLowDelayCheck = 0
-                self.activeCheckTimes = 0
-                if self.debug:
-                    print(
-                    f"{Colors.OKCYAN}  Pin {self.sensorPin.readPinName()} delay has passed of more than {self.delayCheck}s RESET --- activeCheckTimes: {self.activeCheckTimes} -- {Colors.ENDC}"
-                    )
+                    if self.activeCheckTimes == len(self.intervalsArray):
+                        if self.debug:
+                            print(f"{Colors.FAIL}!!! Pin {self.sensorPin.readPinName()} ALARM SET !!!!{Colors.ENDC}")
+                            print(f"{Colors.FAIL}!!! me shouting")
+                        
+                        #start warning audio not sure yet
+                        #play(self.me_talking)
+                        self.sounds.playSound('me_talking')
+                        
+                        # set relay pin on for alarm if not already on
+                        relayOff = not self.relayOutput1.readPinValue()
+                        if relayOff:
+                            if self.debug:
+                                print(f"{Colors.FAIL}!!! ===> relay set ON")
+                            self.relayOutput1.writeOutPin('HIGH')
+                        
+                        # alarm is on for 10s
+                        # maybe check how many times alarm got off and decrease time for next check ?
+                        sleep(self.alarmIsOnFor)
+                        #turn off the relay
+                        if self.debug:
+                            print(f"{Colors.FAIL}!!! ===> relay set OFF")
+                        self.relayOutput1.writeOutPin('LOW')
+                        # reset everything
+                        self.beenHighTime = 0
+                        self.activeCheckTimes = 0
+
+                # do not use recursive it has limit of 1000 repeats
+                # run method in next second
+                # self.runTimingAlarm()
             else:
-                if self.debug:
-                    print(
-                    f"{Colors.OKGREEN} Pin {self.sensorPin.readPinName()} has been LOW--- activeCheckTimes: {self.activeCheckTimes} -- {Colors.ENDC}"
-                    )
+                # check pin OFF every second
+                sleep(1)
+
+                # sensor Low, set Led OFF if Not already OFF
+                if not ledOff:
+                    self.detectionLed.writeOutPin('LOW')
+                    
+                # always reset beenHighTime cause activeCheckTimes tracks if was active before
+                self.beenHighTime = 0
+
+                if self.activeCheckTimes > 0:
+                    self.beenLowDelayCheck += 1
+                    if self.debug:
+                        print(f"{Colors.OKGREEN} Pin {self.sensorPin.readPinName()} been Low for: {self.beenLowDelayCheck}{Colors.ENDC}")
+                if self.beenLowDelayCheck > self.delayCheck:
+                    self.beenLowDelayCheck = 0
+                    self.activeCheckTimes = 0
+                    if self.debug:
+                        print(
+                        f"{Colors.OKCYAN}  Pin {self.sensorPin.readPinName()} delay has passed of more than {self.delayCheck}s RESET --- activeCheckTimes: {self.activeCheckTimes} -- {Colors.ENDC}"
+                        )
+                else:
+                    if self.debug:
+                        print(
+                        f"{Colors.OKGREEN} Pin {self.sensorPin.readPinName()} has been LOW--- activeCheckTimes: {self.activeCheckTimes} -- {Colors.ENDC}"
+                        )
+
+            # do not use recursive it has limit of 1000 repeats
             # run method in next second
-            self.runTimingAlarm()
+            # self.runTimingAlarm()
